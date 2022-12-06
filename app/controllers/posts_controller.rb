7642bdf4a068f0bfb51c71fc35b9d3b3cbeec20e
箱崎章朗 <hakozakifumiaki@hakozaki-fumiaki-MacBook-Air.local>
2022-12-06 19:42:00 +0900

    編集、削除実装(コミット忘れておりました申し訳ございません)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,46 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
     @post = Post.new
   end
 
-  def edit
+  def create
+    Post.create(post_params)
+    redirect_to new_post_path
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_params, notice: "言い換えました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice:"つぶやきさようなら・・・"
+  end
+
+
   def show
+    @post = Post.find(params[:id])
+  end
+
+
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
